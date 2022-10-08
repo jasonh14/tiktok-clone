e7e0f29d8736737e909ca6eb14bd9623cfc4041f
Jason Hartanto <jason.hartanto@binus.ac.id>
2022-10-08 13:09:07 +0700

    login finish

---
+++ components/NoResults.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NextPage } from "next";
+
+interface IProps {
+  text: string;
+}
+
+const NoResults: NextPage<IProps> = ({ text }) => {
+  return <div>{text}</div>;
+};
+
+export default NoResults;
--- utils/index.ts
+++ utils/index.ts
@@ -1,24 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
+import jwtDecode from "jwt-decode";
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+type Decoded = { name: string; picture: string; sub: string };
 
 export const createOrGetUser = async (response: any, addUser: any) => {
-  var base64Url = response.credential.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-  
-  const { name, picture, sub } = JSON.parse(jsonPayload)
-  
+  const decoded: Decoded = jwtDecode(response.credential);
+  const { name, picture, sub } = decoded;
+
   const user = {
     _id: sub,
-    _type: 'user',
+    _type: "user",
     userName: name,
     image: picture,
   };
-  
+
   addUser(user);
 
-  await axios.post(`${BASE_URL}/api/auth`, user);
+  await axios.post(`http://localhost:3000/api/auth`, user);
 };
